773c100fe406d3df860c5e71228a35d603898678
cuongdeptraivkl <maithanhcuong251003@gmail.com>
2023-08-19 16:16:23 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+
+const router = createBrowserRouter([
+
+])
+function App() {
+  return <RouterProvider router={router} />
+}
+
+export default App
